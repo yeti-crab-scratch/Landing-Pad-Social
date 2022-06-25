2aa50f16279140ce0ac62e8f555684cc9110cfb4
Elliott <Elliott@Elliotts-MacBook-Pro.local>
2022-06-25 16:07:41 -0700

    added patch functionality to the home router and added create new event feature to createEvent router

---
+++ server/routes/createEventRouter.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const eventController = require('../controllers/eventController')
+
+const router = express.Router();
+
+router.post('/', eventController.createEvent, (req, res) => {
+    res.status(200).json(res.locals.newEvent);
+})
+
+module.exports = router;
