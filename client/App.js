3c3100cfe5075b9e55c0ff964011b677633b3187
Johnny Chan <johnnychan2011@gmail.com>
2022-06-25 10:34:28 -0700

    Co-authored-by: Chris Fryer <Frynoceros@users.noreply.github.com> Co-authored-by: Jace Crowe <JaceCrowe@users.noreply.github.com> Co-authored-by: Elliott <elliottburr@users.noreply.github.com>

--- client/App.js
+++ client/App.js
@@ -2,7 +2,15 @@ import React, { Component } from 'react';
 
 class App extends Component {
 
+    
+
     render() {
+
+    fetch("http://localhost:3000/")
+        .then(response => response.json())
+        .then(response => console.log(response));
+
+
         return (
             <div>
                 <h1>Hello world!</h1>
--- server/server.js
+++ server/server.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const cors = require("cors")
 
 const PORT = 3000;
 
+app.use(cors())
+app.use(express.json());
+
 app.get('/', (req,res) => {
     console.log(req.body)
-    res.send("It worked!")
+    res.send({key: "It worked!"})
 })
 
 app.listen(PORT, console.log(`listening on port ${PORT}...`));
