2cb3d967b00a5a51624112ef0b7f0dcb46331df4
Johnny Chan <johnnychan2011@gmail.com>
2022-06-25 12:17:23 -0700

    Updated react router nav  bar

--- client/index.js
+++ client/index.js
@@ -1,9 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-// import styles from './style.css';
+import styles from './style.css';
+
+import { BrowserRouter, Routes, Route, } from "react-router-dom";
+import Home from "./routes/home.jsx";
+import CreateEvent from "./routes/createEvent.jsx";
+
+
 
 ReactDOM.render(
-<App />, 
+ <BrowserRouter>
+    <Routes>
+        <Route path = "/" element = {<App/>}>
+            <Route path = "home" element = {<Home/>}/>
+            <Route path = "createEvent" element = {<CreateEvent/>}/>
+            <Route path="*" element={
+                <main style={{ padding: "1rem" }}>
+                    <p>There's nothing here!</p>
+                </main>
+                 } /* no match case */
+             />
+        </Route>
+    </Routes>
+</BrowserRouter>,
+
 document.getElementById('root')
 );
+
+//nest routes to persist - add Outlet in app.js
