8cf295857ca052ef441b61365e7b19b61f196fa2
Johnny Chan <johnnychan2011@gmail.com>
2022-06-27 00:22:09 -0700

    updated fields for create event page

--- client/routes/createEvent.jsx
+++ client/routes/createEvent.jsx
@@ -1,10 +1,46 @@
 import React from 'react';
+import { useState } from 'react';
+
+
 
 export default function createEvent() {
+  const [title, setTitle] = useState('');
+  const [date, setDate] = useState('');
+  const [timeStart, setTimeStart] = useState('');
+  const [timeEnd, setTimeEnd] = useState('');
+  const [activity_type, setActivity_type] = useState('');
+  const [numParticipants, setNumParticipants] = useState('');
+  const [zip, setZip] = useState('');
+
+
     return(
     <main>
-        <div>
-      <h1 id = "eventContainer">Create Event Page</h1>
+        <div id = "eventContainer">
+         <h1>Create Event Page</h1>
+         <label>Event title: &nbsp;</label> 
+         <input onChange = {e => setTitle(e.target.value)} />
+      <div>
+       <label id = "eventFields">Date: &nbsp;</label> 
+       <input id = "eventFields" onChange = {e => setDate(e.target.value)} />
+      </div>
+      <div>
+       <label id = "eventFields">Time Start: &nbsp;</label> 
+       <input id = "eventFields" onChange = {e => setTimeStart(e.target.value)} />
+
+       <label id = "eventFields">Time End: &nbsp;</label> 
+       <input id = "eventFields" onChange = {e => setTimeEnd(e.target.value)} />
+      </div>
+      <div>
+       <label id = "eventFields">Activity Type: &nbsp;</label> 
+       <input id = "eventFields" placeholder='Drop down menu?' onChange = {e => setActivity_type(e.target.value)} />
+      </div>
+      <div>
+       <label id = "eventFields">Max number participants: &nbsp;</label> 
+       <input id = "eventFields" onChange = {e => setNumParticipants(e.target.value)} />
+      </div>
+
+
+
       </div>
     </main>
     )
